51d44acb157061033b6081537bd2770e65468abe
smhrd <smhrd@220.71.97.187>
2023-09-21 17:49:35 +0900

    main 수정

--- Mini_project/src/Mini_project/Main.java
+++ Mini_project/src/Mini_project/Main.java
@@ -4,30 +4,14 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-
+static MiniDAO dao = new MiniDAO();
+static Controller con = new Controller();
+static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
-		// view
-		Scanner sc = new Scanner(System.in);
-		Controller con = new Controller();
-		MiniDAO dao = new MiniDAO();
-
 		while (true) {
-			System.out.println("1.회원가입 2.로그인 3.전체랭크확인");
-			int n = sc.nextInt();
-			if (n == 1) {
-				System.out.print("id 입력 : ");
-				String id = sc.next();
-				System.out.print("pw 입력 : ");
-				String pw = sc.next();
-				System.out.print("이름 입력 : ");
-				String name = sc.next();
-
-				int row = dao.join(new MiniDTO(name, pw, id));
-				if (row > 0) {
-					System.out.println("완료");
-				} else {
-					System.out.println("실패");
-				}
+			int n= sc.nextInt();
+			if(n == 1) {
+				jjo();
 			} else if (n == 2) {
 				System.out.println("id를 입력해주세요");
 				String id = sc.next();
@@ -49,7 +33,24 @@ public class Main {
 				System.out.println("잘못 입력하셨습니다. 다시 입력해주세요");
 			}
 		}
+	}
+	static void jjo() {
+		System.out.println("1.회원가입 2.로그인 3.전체랭크확인");
+		int n = sc.nextInt();
+			System.out.print("id 입력 : ");
+			String id = sc.next();
+			System.out.print("pw 입력 : ");
+			String pw = sc.next();
+			System.out.print("이름 입력 : ");
+			String name = sc.next();
 
+			int row = dao.join(new MiniDTO(name, pw, id));
+			if (row > 0) {
+				System.out.println("완료");
+			} else {
+				System.out.println("실패");
+			}
+		
 	}
 
 }
