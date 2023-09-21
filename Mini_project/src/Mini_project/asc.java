eb262fe6eae0c19ffd83962b462672b68ba4b9f5
SMHRD <SMHRD@210.183.87.104>
2023-09-21 16:14:27 +0900

    아스키코드 노가다 완, 메소드 완

--- Mini_project/src/Mini_project/asc.java
+++ Mini_project/src/Mini_project/asc.java
@@ -3,4 +3,157 @@ package Mini_project;
 public class asc {
 	//ascii 코드 모음 class
 	//메소드로 구성
+	
+	public void 부장님() {
+		
+		System.out.println("⠀⠀⠀⠀⢀⣴⠶⠿⠟⠛⠻⠛⠳⠶⣄⡀");
+		System.out.println("⠀⠀⣠⣶⣿⣿⣿⣶⣖⠶⢶⣤⡀⠀⠈⢿⣆");
+		System.out.println("⢀⣴⣿⠋⠉⠉⠀⠀⠈⠉⠛⠿⢿⣷⡀⠀⠈⢷⡀");
+		System.out.println("⡾⠉⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢦⡀⠘⣷⡀");
+		System.out.println("⣷⢰⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢳⡀⢸⡇");
+		System.out.println("⢻⡜⡄⠀⢀⣀⣤⣶⣶⡄⣴⣾⣿⣛⣓⠀⠀⣧⢸⣇");
+		System.out.println("⢈⣧⣧⠀⢩⠞⠿⠿⠻⠀⠘⠙⠃⠛⠛⠓⠀⣿⣻⠿⣷");
+		System.out.println("⢸⡵⣿⠀⠀⠀⠀⠀⠀⠀⠀⠠⡀⠀⠀⠀⠀⠀⢻⣇⡟");
+		System.out.println("⠘⢧⣿⡀⠀⠀⠀⠀⢧⣤⣤⣶⣗⠀⠀⠀⠀⠀⠜⣽⠁");
+		System.out.println("⠀⠈⢿⣧⠀⠀⠀⠀⣿⣿⣿⣿⣿⣀⠀⠀⠀⢠⡟⠁");
+		System.out.println("⠀⠀⠀⠘⣇⠀⠀⠰⠋⠉⠙⠂⠀⠉⠀⠀⠀⣼⡅");
+		System.out.println("⠀⠀⠀⠀⠹⣦⡀⠀⠀⠀⠉⠉⠁⠀⠀⠀⣠⠏⢻⣤⡀");
+		System.out.println("⠀⠀⠀⠀⠀⢹⡷⢦⣄⣀⣀⣀⣀⣤⣴⡾⠃⠀⠘⡿⠙⢶");
+		System.out.println("⠀⠀⠀⠀⠀⢨⡷⣤⡀⠈⠉⠉⢁⡴⠋⠀⠀⠀⣸⠃");
+		
+	}
+	
+	public void 박수() {
+		
+		System.out.println("   ⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⢀⡠⣄⡀⠀⠀⡠⠞⠛⢦⣠⢤⡀");
+		System.out.println("  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⢠⠏⠀⠀⢱⡀⣸⠁⠀⡴⠋⠀⠀⣹");
+		System.out.println("⠀⠀  ⠀⠀⠀⠀⠀⠀⠀⠀⡴⠋⠉⢿⢀⡤⠶⣴⠇⣯⠀⣼⠁⠀⢀⡴⠷⣄");
+		System.out.println("⠀⠀  ⠀⠀⠀⠀⠀⠀⢠⠞⠁⠀⣀⡾⠋⠀⠀⢹⣼⠁⢠⡇⠀⡴⠋⠀⠀⡼");
+		System.out.println("⠀⠀ ⠀⢠⠊⠑⢦⠀⡴⠋⢀⣠⠞⠉⠀⠀⠀⣠⣿⠧⣄⡾⠁⡼⠁⣀⣤⠾⡁");
+		System.out.println("⠀⠀ ⠀⢸⠀⠀⣨⠟⠁⢠⡞⠁⠀⠀⠀⣠⡾⠛⠁⠀⣿⠃⣰⠃⣴⠋⠀⠀⣷");
+		System.out.println("⠀ ⠀⠀⣸⢠⠞⠁⠀⢠⠏⠀⠀⢀⡴⠋⠁⠀⢀⣠⡴⠿⣶⡇⢰⠇⠀⠀⢠⠇");
+		System.out.println("⠀ ⠀⢠⢿⠏⠀⠀⠀⠉⠀⠀⣠⠞⠁⠀⡴⠚⠉⠁⠀⢀⡟⠀⣼⠀⠀⠀⢸");
+		System.out.println("⠀ ⠀⡾⣼⢀⠀⠀⠀⠀⠀⠈⠉⠀⣠⠞⠁⠀⠀⢀⡴⠋⠙⢼⠃⠀⠀⠀⣸");
+		System.out.println("⠀ ⠀⡇⠉⡎⠀⣰⠃⠀⠀⠀⠀⠀⠁⠀⠀⠀⡼⠉⠀⠀⠀⠘⠂⠀⠀⣠⠇");
+		System.out.println("⠀ ⠀⡇⢸⠀⣰⠃⠀⡴⠀⠀⠀⠀⠀⠀⣠⠞⠁⠀⠀⠀⠀⠀⠀⣠⠖⠁");
+		System.out.println("⠀⠀⢸⠁⡏⢠⠃⢀⠞⠀⠀⠀⠀⠀⠀⢸⠁⠀⠀⠀⠀⢀⣠⠖⠋⠁");
+		System.out.println("⠀⠀⡞⠀⠃⡎⢀⠏⠀⠀⠀⠀⠀⠀⢀⡏⠀⣀⡤⠴⠚⠉");
+		System.out.println("⡴⢺⠇⠀⠀⠀⠞⠀⠀⠀⠀⠀⠀⢀⡾⠒⠋⠁");
+		System.out.println("⡇⠘⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠞");
+		System.out.println("⢳⡀⠘⢦⡀⠀⠀⠀⠀⠀⠀⡰⠋");
+		System.out.println(" ⠳⣄⠀⠙⠲⣤⣀⣠⠴⠊");
+		System.out.println("  ⠀⠈⠓⠦⣄⣀⡠⠎");
+		
+	}
+	
+	public void GOOD() {
+		
+		System.out.println("   █████████    ███████      ███████  ██████████");
+		System.out.println("  ███░░░░░███ ███░░░░░███  ███░░░░░██░░███░░░░███");
+		System.out.println(" ███     ░░░ ███     ░░██████     ░░██░███   ░░███");
+		System.out.println("░███        ░███      ░██░███      ░██░███    ░███");
+		System.out.println("░███    ████░███      ░██░███      ░██░███    ░███");
+		System.out.println("░░███  ░░███░░███     ███░░███     ███░███    ███");
+		System.out.println(" ░░█████████ ░░░███████░  ░░░███████░ ██████████");
+		System.out.println("  ░░░░░░░░░    ░░░░░░░      ░░░░░░░  ░░░░░░░░░░");
+		
+	}
+	
+	public void Bad() {
+		
+		System.out.println(" ███████████             █████");
+		System.out.println("░░███░░░░░███           ░░███");
+		System.out.println(" ░███    ░█████████   ███████");
+		System.out.println(" ░██████████░░░░░███ ███░░███");
+		System.out.println(" ░███░░░░░██████████░███ ░███");
+		System.out.println(" ░███    ░█████░░███░███ ░███");
+		System.out.println(" ██████████░░███████░░████████");
+		System.out.println("░░░░░░░░░░░ ░░░░░░░░ ░░░░░░░░");
+		
+	}
+	
+	public void 고기() {
+		
+		System.out.println("                       ⣀⣤⡴⠄⠀⠘⠛⠷⠶⠶⣦⣄⡀");
+		System.out.println("                  ⢀⣀⣤⣤⠶⠛⠋⠁⠀⢀⣀⣤⣤⣤⣀⠀⠀⠈⠻⣦");
+		System.out.println("   ⣠⡴⠶⠞⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠋⠉⠁⠀⠀⢀⣀⣴⣾⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⢹⡇");
+		System.out.println(" ⣠⡿⠉⠀⠀⣀⣤⣤⣤⣤⣄⣀⠀⠀⠀⠀⣠⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⣸⡇");
+		System.out.println("⢠⡿⠀⠀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⢠⣿⡇");
+		System.out.println("⢸⡇⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⡀⠀⠉⠛⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀⣠⡿⢻⡇");
+		System.out.println("⢸⣷⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣤⣤⣄⡀⠉⠉⠛⠛⠛⠉⠁⠀⢀⣼⠟⠀⣼⡇");
+		System.out.println("⢸⣿⣧⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⠀⠀⠀⠀⣠⣴⠟⠁⠀⢰⡿");
+		System.out.println("⢸⡇⠙⢷⣄⠀⠀⠈⠙⠿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠟⠛⠁⠀⣀⣴⡾⠛⠁⠀⠀⣰⡿⠁");
+		System.out.println(" ⠸⣷⠀⠀⠙⠿⣦⣤⣀⠀⠀⠀⠀⠈⠉⠉⠉⠉⠁⠀⠀⠀⢀⣠⣤⣶⠿⠛⠁⠀⠀⠀⢀⣴⠟⠁");
+		System.out.println("  ⠻⡧⠀⠀⠀⠀⠉⠙⠛⠿⠿⠶⠶⠶⠶⠶⠶⠶⠿⠿⠛⠛⠉⠁⠀⠀⠀⠀⠀⢀⣴⠟⠁");
+		System.out.println("    ⢴⣄⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⢀⠀⢠⣤⣶⡶⠀⠀⣀⣤⡾⠟⠁");
+		System.out.println("     ⠀⣤⣄⣀⠀⠛⠿⠿⠿⠶⠿⠿⠿⠛⠃⠀⢁⣀⣤⣶⠾⠛⠉");
+		System.out.println("        ⠉⠛⠻⠿⠶⠶⢶⣶⣶⣶⠶⠶⠶⠿⠟⠛⠋⠁");
+		
+	}
+	
+	public void 소스() {
+		
+		System.out.println("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡤⠶⢤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+		System.out.println("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠋⠀⠀⠀⠙⢢⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+		System.out.println("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠞⣠⠀⠀⠀⠀⠀⠀⠙⢦⡀⠀⠀⠀⠀⠀⠀⠀");
+		System.out.println("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠞⢁⠄⠁⠀⠀⠀⠀⠀⠀⠀⠀⠉⣆⠀⠀⠀⠀⠀⠀");
+		System.out.println("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠔⢁⠔⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠏⠀⠀⠀⠀⠀⠀");
+		System.out.println("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡴⠋⡠⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠞⠁⠀⠀⠀⠀⠀⠀⠀");
+		System.out.println("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠋⠠⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+		System.out.println("⠀⠀⠀⠀⠀⠀⠀⠀⣠⠞⣁⠖⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡔⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+		System.out.println("⠀⠀⠀⠀⠀⠀⢀⠞⢁⠔⠁⠀⠀⠀⠀⠀⠀⠀⠀⣠⠜⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+		System.out.println("⠀⠀⠀⢀⡞⢉⠣⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+		System.out.println("⠀⠀⠀⠈⢳⣌⠳⣌⠲⣀⠀⠀⠀⠀⢀⡰⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+		System.out.println("⠀⠀⠀⢠⡏⠈⠳⣌⠳⠌⠣⡄⢀⠴⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+		System.out.println("⠀⠀⠀⣨⠷⣄⠀⠈⣳⣄⠀⢈⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+		System.out.println("⠀⢀⡴⣁⠴⠊⠓⠊⠁⠈⠓⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+		System.out.println("⠀⠻⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+		System.out.println("⠈⠉⠛⠲⢤⣀⣀⣀⣀⠤⠚⠋⠉⠉⠉⠓⠢⣄⣀⣀⣀⣠⠔⠚⠉⠉⠉⠙⠓⠦⣀⣀");
+		
+	}
+	
+	public void 쌈() {
+		
+		System.out.println("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⢀⣀⠀⠀⠀⠀⠀⠀⠀⠀ ");
+		System.out.println("⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⣤⣤⣤⣰⣿⣿⣦⣄⡀⠀⠀⠀⠀⠀ ");
+		System.out.println("⠀⠀⠀⣠⣤⣀⣰⣿⣿⣶⣾⣇⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀ ");
+		System.out.println("⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀");
+		System.out.println("⠀⠀⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇");
+		System.out.println("⢠⣶⣿⣿⣿⣿⣟⣿⣿⣿⣿⣿⡟⡾⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄");
+		System.out.println("⠘⣿⣿⣿⣿⣿⣿⣾⠿⣿⣿⡿⢃⣼⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠉⠀⠀");
+		System.out.println("⢀⣼⣿⣿⣿⣿⣿⣿⡟⣿⠋⣠⣶⣿⣭⣽⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀");
+		System.out.println("⠘⣿⣿⣿⣿⡟⣿⣿⠑⠁⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⠁⠀⠀⠀⠀⠀⠀");
+		System.out.println("⠀⠘⣿⣿⣿⡇⡿⠋⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⠀⠀⠀");
+		System.out.println("⠀⢠⣼⣿⡿⠀⠀⠀⠚⢛⡛⠭⣷⣿⣿⣿⣿⣿⣿⣿⡟⠋⠀⠀⠀⠀⠀⠀⠀");
+		System.out.println("⠀⢸⠿⠋⠀⠀⢠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠀⠀⠀⠀⠀⠀⠀⠀");
+		System.out.println("⠀⠁⠀⠀⢀⣴⡿⣿⣿⡏⠙⢿⣿⣿⢿⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+		System.out.println("⠀⠀⠀⠐⠉⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+		
+	}
+	
+	public void 유리컵() {
+		
+		System.out.println("  ⠀⠀⠀⣀⡀⠀⠠⠤⠤⠤⠤⠄⠀ ⢀⣄⠀⠀⠀⠀⠀");
+		System.out.println("⠀⠐⠂⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⠉⠐⠂⠀");
+		System.out.println("⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀ ⢀⣤");
+		System.out.println("⠉⠀⠘⠂⠶⠀⢠⣤⣤⠀⠀⠀⠀⣤⣤⡄⠀⠶⠐⠃⠀⠉⠀");
+		System.out.println("⢰⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀  ⢰⡆");
+		System.out.println("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀ ⠀⠀⣀⠀⠀");
+		System.out.println("⠀⠿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀ ⠀⠛⠀⠀");
+		System.out.println("⠀⢠⡄⠀⠀⠀⣀⣠⣤⣤⣤⣦⣤⣤⣤⣀⡀⠀⠀⢠⡄⠀⠀");
+		System.out.println("⠀⠈⠁⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⠈⠁⠀");
+		System.out.println("⠀⠀⣶⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠉⣶⠀⠀");
+		System.out.println("⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀");
+		System.out.println("⠀⠀⠸⠇⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡯⠙⠃⠀⠀");
+		System.out.println("⠀⠀⠀⣄⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣤⠀⠀⠀");
+		System.out.println("⠀⠀⠀⠋⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠉⠉⠀⠀⠀");
+		System.out.println("⠀⠀⠀⢰⡼⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡷⡇⠀⠀⠀");
+		System.out.println("⠀⠀⠀⠈⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⢀⠀⠀⠀⠀");
+		System.out.println("⠀⠀⠀⠀⠿⢙⡉⠿⠛⠛⢿⣿⠟⠛⠻⢋⠫⠛⠀⠀⠀⠀");
+		System.out.println("⠀⠀⠀⠀⠀⠀⠀⠉⠀⠐⠒⠒⠂⠀⠉⠀⠀⠀⠀⠀⠀⠀⠀");
+		
+	}
+	
+	
+	
 }
