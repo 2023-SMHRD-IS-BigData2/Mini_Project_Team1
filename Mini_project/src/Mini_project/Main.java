e976450626d6eaf24a5d249b9f09da49121f62f4
SMHRD <SMHRD@210.183.87.104>
2023-09-21 17:54:30 +0900

    gggg

--- Mini_project/src/Mini_project/Main.java
+++ Mini_project/src/Mini_project/Main.java
@@ -4,53 +4,65 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-static MiniDAO dao = new MiniDAO();
-static Controller con = new Controller();
-static Scanner sc = new Scanner(System.in);
+
+	static MiniDAO dao = new MiniDAO();
+	static Controller con = new Controller();
+	static Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		while (true) {
-			int n= sc.nextInt();
-			if(n == 1) {
+			int n = sc.nextInt();
+			if (n == 1) {
 				jjo();
 			} else if (n == 2) {
-				System.out.println("id를 입력해주세요");
-				String id = sc.next();
-				System.out.println("비밀번호를 입력해주세요");
-				String pw = sc.next();
-				boolean isLogin = dao.login(id, pw);
-				if (isLogin == true) {
-					System.out.println("로그인 성공");
-				} else {
-					System.out.println("로그인 실패");
-				}
+				llo();
 			} else if (n == 3) {
-				ArrayList<MiniDTO> li = con.returnRankAll();
-				for (int i = 0; i < li.size(); i++) {
-					System.out.print(i + 1 + "등 : " + li.get(i).getId() + " ");
-					System.out.println(li.get(i).getScore());
-				}
+				sso();
 			} else {
 				System.out.println("잘못 입력하셨습니다. 다시 입력해주세요");
 			}
 		}
 	}
+
 	static void jjo() {
 		System.out.println("1.회원가입 2.로그인 3.전체랭크확인");
 		int n = sc.nextInt();
-			System.out.print("id 입력 : ");
-			String id = sc.next();
-			System.out.print("pw 입력 : ");
-			String pw = sc.next();
-			System.out.print("이름 입력 : ");
-			String name = sc.next();
+		System.out.print("id 입력 : ");
+		String id = sc.next();
+		System.out.print("pw 입력 : ");
+		String pw = sc.next();
+		System.out.print("이름 입력 : ");
+		String name = sc.next();
 
-			int row = dao.join(new MiniDTO(name, pw, id));
-			if (row > 0) {
-				System.out.println("완료");
+		int row = dao.join(new MiniDTO(name, pw, id));
+		if (row > 0) {
+			System.out.println("완료");
 			} else {
-				System.out.println("실패");
-			}
-		
+			System.out.println("실패");
+		}
+	}
+
+	static void llo() {
+
+		System.out.println("id를 입력해주세요");
+		String id = sc.next();
+		System.out.println("비밀번호를 입력해주세요");
+		String pw = sc.next();
+		boolean isLogin = dao.login(id, pw);
+		if (isLogin == true) {
+			System.out.println("로그인 성공");
+			} else {
+			System.out.println("로그인 실패");
+		}
+	}
+
+	static void sso() {
+
+		ArrayList<MiniDTO> li = con.returnRankAll();
+		for (int i = 0; i < li.size(); i++) {
+			System.out.print(i + 1 + "등 : " + li.get(i).getId() + " ");
+			System.out.println(li.get(i).getScore());
+		}
 	}
 
 }
