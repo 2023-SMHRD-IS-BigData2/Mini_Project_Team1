e8b8035aa4982d76bd427405daa0d7fbc7e62684
SMHRD <SMHRD@210.183.87.104>
2023-09-22 18:47:56 +0900

    우왕

--- Mini_project/src/Mini_project/Controller.java
+++ Mini_project/src/Mini_project/Controller.java
@@ -1,61 +1,48 @@
 package Mini_project;
 
-import java.util.ArrayList;
-import Controller.MusicVO;
 import javazoom.jl.player.MP3Player;
-import Controller.MP3Play;
 
 public class Controller {
-	MiniDAO dao = new MiniDAO();
-	private MuseicVO[] music;
-	private MP3Player player;
 
-	private int currentIndex;
+	MP3Player player;
 
-	public Controller() {
-		music = new MusicVO[4];
-	}
+	public void 시작노래() {
 
-	music[0]=new MusicVO("긴장감 브금.mp3");music[1]=new MusicVO("호응+휘파람.mp3");music[2]=new MusicVO("좌절 브금(두둥..).mp3");music[3]=new MusicVO("씹는 소리.mp3");
-
-	player=new MP3Player();currentIndex=0;
-
-}
-
-	}
-
-	public void play() {
 		if (player.isPlaying()) {
 			player.stop();
 		}
-		player.play(music[currentIndex].getWay());
-	}
 
-	public void next() {
-		currentIndex = (currentIndex == music.length - 1) ? 0 : currentIndex + 1;
-		play();
-	}
-
-	public void stop() {
-		player.stop();
+		player.play("긴장감 브금.mp3");
 
 	}
 
-	public void pre() {
-		// 방법 1
-		currentIndex = (currentIndex == 0) ? music.length - 1 : currentIndex - 1;
-		play();
+	public void 성공노래() {
+
+		if (player.isPlaying()) {
+			player.stop();
+		}
+
+		player.play("호응+휘파람.mp3");
 
 	}
 
-	public String nowplay() {
-		return music[currentIndex].toString();
+	public void 실패노래() {
+
+		if (player.isPlaying()) {
+			player.stop();
+		}
+
+		player.play("좌절 브금(두둥..).mp3");
+
 	}
 
-	public ArrayList<MiniDTO> returnRankAll() {
-		ArrayList<MiniDTO> li = new ArrayList<>();
-		li = dao.rankAll();
-		return li;
-	}
+	public void 먹는소리() {
 
+		if (player.isPlaying()) {
+			player.stop();
+		}
+
+		player.play("씹는 소리.mp3");
+
+	}
 }
--- Mini_project/src/Mini_project/MusicVO.java
+++ Mini_project/src/Mini_project/MusicVO.java
@@ -1,5 +1,20 @@
 package Mini_project;
 
 public class MusicVO {
+	
+	// 필드
+	private String path;
+	
+	// 생성자
+	public 음악VO(String path) {
+		this.path = path;
+	}
+
+	// get
+	public String getPath() {
+		return path;
+	}
+	
+}
 
 }
