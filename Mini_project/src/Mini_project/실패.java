dc1264ceef6b1ae4831d204441a7dd7bc4b4adf8
user <user@218.149.140.118>
2023-09-22 12:02:00 +0900

    실패 메서드

---
+++ Mini_project/src/Mini_project/실패.java
@@ -0,0 +1,64 @@
+package Mini_project;
+
+import java.util.Scanner;
+
+public class 실패 {
+
+}
+
+
+
+
+
+
+talk t = new talk();
+Controller con = new Controller();
+MiniDAO dao = new MiniDAO();
+MiniDTO dto = new MiniDTO();
+
+	public void 상() {
+
+		t.FailScenario();
+
+	}
+
+	public void 중() {
+
+		t.NormalFailScenario();
+
+	}
+
+	public void 하() {
+
+		t.EasyFailScenario();
+
+	}
+
+	private void fail () {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("1.종료 2.재시작 3.랭킹확인");
+		System.out.println("번호 입력 >> ");
+		int n = sc.nextInt();
+		
+
+		
+		if (n == 1) {
+			System.out.println("게임을 종료합니다.");
+		} else if (n == 2) {
+			
+		} else if (n == 3) {
+
+			String id = dto.getId();
+
+			int num = rankSingle(id);
+				
+			}
+		} else {
+			System.out.println("잘못된 선택입니다. 다시 입력해주세요.");
+
+		}
+	}
+
+}
